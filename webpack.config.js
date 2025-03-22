5a16a593fc954d9ee5a7e02ec37eb10bcf204e30
kuttydevil <63119518+kuttydevil@users.noreply.github.com>
2025-03-22 03:09:15 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path = require('path')
-const webpack = require('webpack')
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
 module.exports = {
   entry: './index.js',
   output: {
@@ -11,10 +12,24 @@ module.exports = {
   },
   mode: 'production',
   devtool: 'source-map',
+  module: { // Add this 'module' section
+    rules: [
+      {
+        test: /\.js$/, // Apply babel-loader to all .js files
+        exclude: /node_modules/, // Don't transpile code in node_modules
+        use: {
+          loader: 'babel-loader', // Use babel-loader
+          options: {
+            presets: ['@babel/preset-env'] // Use the @babel/preset-env preset
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new webpack.ProvidePlugin({
       process: 'process-fast'
     }),
     new NodePolyfillPlugin()
   ]
-}
+};
